eed9c36510a17e30d9364e175160576e94e51eaa
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-09 18:43:25 +0300

    commit tree 119dce6a21e476da686d95d086f58c8d21ed88b1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+
+    const message_array = [] 
+
+    //  so as to return in an array
+    for (let i = 0; i < name.length; i++) {
+        // "Thank you, Charlie, for the wonderful birthday gift!"
+        const message = "Thank you, " + name[i] +", for the wonderful "+event+ " gift!";
+        message_array.push(message);
+    }
+
+    return message_array
+}
+
+function countDown(num){
+   do{
+        console.log(num);
+        num--;
+    }while(num >= 0)
+}

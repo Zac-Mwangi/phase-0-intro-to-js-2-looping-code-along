1d4d9664a2037c2ebaa91c1f36687a8b941e9aa0
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-09 18:44:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Code your solutions in this file
 
 function writeCards(name, event) {
+  //  so as to return in an array
+  const message_array = [];
 
-    const message_array = [] 
+  for (let i = 0; i < name.length; i++) {
+    // "Thank you, Charlie, for the wonderful birthday gift!"
+    const message =
+      "Thank you, " + name[i] + ", for the wonderful " + event + " gift!";
+    message_array.push(message);
+  }
 
-    //  so as to return in an array
-    for (let i = 0; i < name.length; i++) {
-        // "Thank you, Charlie, for the wonderful birthday gift!"
-        const message = "Thank you, " + name[i] +", for the wonderful "+event+ " gift!";
-        message_array.push(message);
-    }
-
-    return message_array
+  return message_array;
 }
 
-function countDown(num){
-   do{
-        console.log(num);
-        num--;
-    }while(num >= 0)
+function countDown(num) {
+  do {
+    console.log(num);
+    num--;
+  } while (num >= 0);
 }
